932fcfd5bdf7ec93f67d68678929fc2481bc20bf
suke <suke@suke-blog.com>
2017-09-08 19:59:32 +0900

    change shell script for supporting UTF-8.

--- gmapsupp_make.sh
+++ gmapsupp_make.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # 1:generate map without contour, 2:with contour
-TARGET_TYPE=2
+TARGET_TYPE=1
+
+# character code ASCII:for Romaji output, UTF-8:for Japanese, Shift-JIS:for Japanese
+MAP_CHARCODE="UTF-8"
+SEARCH_CHARCODE="UTF-8"
 
 BASE_DIR=$(cd $(dirname $0) && pwd)
 SOURCE_URL="http://download.geofabrik.de/asia/japan-latest.osm.pbf"
@@ -48,23 +52,38 @@ eval "cp ../areas.list ."
 $SPLITTER --keep-complete=true --description="OSM Japan" --max-nodes=1000000 --search-limit=10000000 --resolution=12 --split-file=areas.list --mapid=${MAPID_PREFIX}0001 $MAP_FILE_ORG
 
 #Convert Kanji to Roman character
-for file in `find . -name "*.osm.pbf"`; do
-	osmconvert $file | perl ../rep_list_pre.pl | iconv -c -f UTF-8 -t SHIFT-JIS | kakasi -Ha -Ka -Ja -Ea -ka $DIC_FILE | perl -pi.bak -e "s/\^/-/g" > ${file}.roman
-done
+if [ $MAP_CHARCODE = 'ASCII' -o $SEARCH_CHARCODE = 'ASCII' ]
+then
+	for file in `find . -name "*.osm.pbf"`; do
+		osmconvert $file | perl ../rep_list_pre.pl | iconv -c -f UTF-8 -t SHIFT-JIS | kakasi -Ha -Ka -Ja -Ea -ka $DIC_FILE | perl -pi.bak -e "s/\^/-/g" > ${file}.roman
+	done
+fi
 
+# generate config for map.
+if [ $MAP_CHARCODE = 'ASCII' ]
+then
+	cat template.args | sed -e "s/\.osm\.pbf/\.osm\.pbf\.roman/g;" > $MKGMAPARGS
+else
+	cat template.args > $MKGMAPARGS
+fi
+cat ../tmz_${MAP_CHARCODE}.args $MKGMAPARGS > japan.args
 
-cat template.args | sed -e "s/\.osm\.pbf/\.osm\.pbf\.roman/g;" > $MKGMAPARGS
-cat ../tmz.args $MKGMAPARGS > japan.args
+# generate config for search.
+if [ $SEARCH_CHARCODE = 'ASCII' ]
+then
+	cat template.args | sed -e "s/\.osm\.pbf/\.osm\.pbf\.roman/g;"  | sed -e "s/mapname: [0-9][0-9][0-9][0-9]/mapname: ${MAPID_SEARCH_PREFIX}/g;" > $MKGMAP_SEARCHARGS
+else
+	cat template.args | sed -e "s/mapname: [0-9][0-9][0-9][0-9]/mapname: ${MAPID_SEARCH_PREFIX}/g;" >  $MKGMAP_SEARCHARGS
+fi
+cat ../tmz_search_${SEARCH_CHARCODE}.args $MKGMAP_SEARCHARGS > japan_search.args
 
-cat $MKGMAPARGS | sed -e "s/mapname: [0-9][0-9][0-9][0-9]/mapname: ${MAPID_SEARCH_PREFIX}/g;" > $MKGMAP_SEARCHARGS
-cat ../tmz_search.args $MKGMAP_SEARCHARGS > japan_search.args
-
+# generate maps
 if [ $TARGET_TYPE -eq "0" ]
 then
   $MKGMAP -c japan_search.args ../tmz_search.typ
   mv gmapsupp.img gmapsupp_search.img
   $MKGMAP -c japan.args ../tmz.typ
-  $MKGMAP -c ../tmz.args ${MAPID_PREFIX}*.img ../tmz.typ ../contour/*.osm ../contour/*.osm.pbf
+  $MKGMAP -c ../tmz_${MAP_CHARCODE}.args ${MAPID_PREFIX}*.img ../tmz.typ ../contour/*.osm ../contour/*.osm.pbf
 elif [ $TARGET_TYPE -eq "1" ]
 then
   $MKGMAP -c japan.args ../tmz.typ
